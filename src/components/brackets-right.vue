e6bd7679c4b50216e0b3a7ba46bc59c52defcb24
Secretlovez <psecloz@qq.com>
2022-09-07 11:17:01 +0800

    feat: 添加标记键值功能

---
+++ src/components/brackets-right.vue
@@ -0,0 +1,53 @@
+<template>
+  <div v-show="dataVisible">
+    <span
+      :class="`vjs-tree__brackets ${bracketsClass}`"
+      @click.stop="toggleBrackets"
+    >
+      {{ bracketsFormatter(Array.isArray(data) ? "]" : "}") }}
+    </span>
+    <span v-if="signComment" class="vjs-comment">
+      {{ signComment }}
+    </span>
+  </div>
+</template>
+
+<script>
+import bracketsMixin from "src/mixins/brackets-mixin";
+
+export default {
+  mixins: [bracketsMixin],
+  props: {
+    bracketsClass: {
+      type: String,
+      default: ""
+    },
+    currentKey: {
+      type: [Number, String],
+      default: ""
+    },
+    showSignComment: {
+      type: Boolean,
+      default: true
+    },
+    // 标记键值对
+    signKeys: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    signComment() {
+      let signValue = "";
+      if (this.signKeys && this.signKeys[this.currentKey] !== undefined) {
+        signValue = ` // ${this.signKeys[this.currentKey]}`;
+      }
+      return this.showSignComment && signValue;
+    },
+    // 当前数据类型
+    dataType() {
+      return getDataType(this.data);
+    }
+  }
+};
+</script>
---
+++ src/components/simple-text.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <slot />
+    <span
+      v-if="customValueFormatter"
+      :class="valueClass"
+      v-html="customFormatter(data)"
+    />
+    <span v-else :class="valueClass">{{ defaultFormatter(data) }}</span>
+    <span v-if="showComma" :class="valueClass">,</span>
+    <span v-if="signComment" class="vjs-comment"> {{ signComment }}</span>
+  </div>
+</template>
+
+<script>
+import { getDataType } from "src/utils";
+
+export default {
+  props: {
+    showDoubleQuotes: Boolean,
+    parentData: {
+      type: [String, Number, Boolean, Array, Object],
+      default: null
+    },
+    data: {
+      type: [String, Number, Boolean],
+      default: ""
+    },
+    showComma: Boolean,
+    currentKey: {
+      type: [Number, String],
+      default: ""
+    },
+    parentKey: {
+      type: [Number, String],
+      default: ""
+    },
+    showSignComment: {
+      type: Boolean,
+      default: true
+    },
+    // 标记键值对
+    signKeys: {
+      type: Object,
+      default: null
+    },
+    customValueFormatter: {
+      type: Function,
+      default: null
+    }
+  },
+  computed: {
+    valueClass() {
+      let signClass = "";
+      // 如果当前键值为标记键值对，则添加标记类
+      if (this.signKeys && this.signKeys[this.currentKey] !== undefined) {
+        signClass = "vjs-value__sign";
+      }
+      // 如果父级键值为标记键值对，则添加标记类
+      if (
+        this.signKeys &&
+        this.parentKey &&
+        this.signKeys[this.parentKey] !== undefined
+      ) {
+        signClass = "vjs-value__sign";
+      }
+      return `vjs-value vjs-value__${this.dataType} ${signClass}`;
+    },
+    signComment() {
+      // 如果当前键值为标记键值对，且展示标记注释，则返回标记注释
+      let signValue = "";
+      if (this.signKeys && this.signKeys[this.currentKey] !== undefined) {
+        signValue = ` // ${this.signKeys[this.currentKey]}`;
+      }
+      return this.showSignComment && signValue;
+    },
+    // 当前数据类型
+    dataType() {
+      return getDataType(this.data);
+    }
+  },
+  methods: {
+    defaultFormatter(data) {
+      let text = data;
+      if (this.dataType === "string") text = `"${text}"`;
+      return text;
+    },
+
+    customFormatter(data) {
+      return this.customValueFormatter
+        ? this.customValueFormatter(
+            data,
+            this.currentKey,
+            this.parentData,
+            this.defaultFormatter(data)
+          )
+        : this.defaultFormatter(data);
+    }
+  }
+};
+</script>
